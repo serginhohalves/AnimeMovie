ff329200b7bb94f555619ce5454ce999de49da21
serginhohalves <80188778+serginhohalves@users.noreply.github.com>
2021-09-04 19:16:27 -0300

    Chamada Api

--- app/src/main/java/com/grupo6cineview/animemovies/api/TMDBApi.kt
+++ app/src/main/java/com/grupo6cineview/animemovies/api/TMDBApi.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface TMDBApi {
-    @GET("movie/now_playing")
+    @GET("movie/now_playing?api_key=055510d3d6b7f9fe9f8ba9c254048055")
     fun getNowPlayingMovies(): Response<NowPlaying>
 }
 
